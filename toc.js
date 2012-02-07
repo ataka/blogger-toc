215d6db5f101e6cc07d4022ff00c95df582447e9
Masayuki Ataka <masayuki.ataka+gravatar@gmail.com>
2012-02-08 03:52:00 +0900

    Use p element instead of list element.
    list elements are applied non-list style in sidebar in some templates.
    It seems bad to me that change style of default template.
    So, simply, list up the titles in p element, with separator 'black star'.
    
    Signed-off-by: Masayuki Ataka <masayuki.ataka+gravatar@gmail.com>

--- toc.js
+++ toc.js
@@ -3,11 +3,14 @@ var titles = d.getElementsByClassName('entry-title');
 var max = titles.length;
 var toc = d.getElementById('toc');
 for (i=0; i<max; i++){
+  // Add Header aka. separator
+  var header = d.createTextNode('★');
+  toc.appendChild(header);
   var a = d.createElement('a');
+  // Add Post Title
   var post_title = titles[i];
   var url = post_title.getElementsByTagName('a')[0];
   a.innerText = post_title.innerText;
   a.setAttribute('href', url.getAttribute('href'));
-  var li = d.createElement('li');
-  toc.appendChild(li.appendChild(a));
+  toc.appendChild(a);
 }
