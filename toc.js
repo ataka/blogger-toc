7d3dbb01fe3548155d22f0a873b9c6997ec0c7a3
Masayuki Ataka <masayuki.ataka+gravatar@gmail.com>
2012-02-08 06:28:31 +0900

    Do not display toc widget when item page.
    If the number of post-entry is less than 1,
    set display style 'none'.
    So, if the label page has only one entry, toc widget is not displayed.
    
    Signed-off-by: Masayuki Ataka <masayuki.ataka+gravatar@gmail.com>

--- toc.js
+++ toc.js
@@ -2,15 +2,19 @@ var d = document;
 var titles = d.getElementsByClassName('entry-title');
 var max = titles.length;
 var toc = d.getElementById('toc');
-for (i=0; i<max; i++){
-  // Add Header aka. separator
-  var header = d.createTextNode('★');
-  toc.appendChild(header);
-  var a = d.createElement('a');
-  // Add Post Title
-  var post_title = titles[i];
-  var url = post_title.getElementsByTagName('a')[0];
-  a.innerText = post_title.innerText;
-  a.setAttribute('href', url.getAttribute('href'));
-  toc.appendChild(a);
+if (max <= 1){
+  toc.parentNode.parentNode.setAttribute('style', 'display:none;');
+} else {
+  for (i=0; i<max; i++){
+    // Add Header aka. separator
+    var header = d.createTextNode('★');
+    toc.appendChild(header);
+    var a = d.createElement('a');
+    // Add Post Title
+    var post_title = titles[i];
+    var url = post_title.getElementsByTagName('a')[0];
+    a.innerText = post_title.innerText;
+    a.setAttribute('href', url.getAttribute('href'));
+    toc.appendChild(a);
+  }
 }
